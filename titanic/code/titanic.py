05a608071cc473ac4fc2d48f9ace0dc845aa9ede
cbchun <cbchun@lsis.biz>
2020-11-10 13:10:50 +0900

    first commit

---
+++ titanic/code/titanic.py
@@ -0,0 +1,14 @@
+#%%
+import pandas as pd
+from sklearn import *
+
+#%%
+gender_submission = pd.read_csv("..//data//gender_submission.csv")
+test = pd.read_csv("..//data//test.csv")
+train = pd.read_csv("..//data//train.csv")
+
+# %%
+lr = linear_model.LogisticRegression(random_state=0)
+lr.fit(train, test)
+lr.score(train, test)
+# %%
